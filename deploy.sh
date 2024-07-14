2846e951cbf84eff270b67df0665a4b5fce273d7
Sviblov <igor.fedotov89@gmail.com>
2024-07-14 22:25:41 +0000

    frontend and backend deployment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ sudo docker-compose -f /home/ubuntu/TG_Bot_Boilerplate/docker-compose.yml down
 
 git -C /home/ubuntu/TG_Selflove_bot pull
 
+#build react project
+
+npm --prefix /home/ubuntu/G_Bot_Boilerplate/frontend run build
+
 #up containers again
 
 sudo docker-compose -f /home/ubuntu/TG_Bot_Boilerplate/docker-compose.yml up --build -d
