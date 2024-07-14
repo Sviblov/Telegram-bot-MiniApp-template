e8f6a5dc5c7f4202e758d73365890d63a1452dc0
Sviblov <igor.fedotov89@gmail.com>
2024-07-14 22:52:47 +0000

    docker compose creation

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo docker-compose -f /home/ubuntu/TG_Bot_Boilerplate/docker-compose.yml down
+sudo docker compose -f /home/ubuntu/TG_Bot_Boilerplate/docker-compose.yml down
 
 git -C /home/ubuntu/TG_Selflove_bot pull
 
 #build react project
 
-npm --prefix /home/ubuntu/G_Bot_Boilerplate/frontend run build
+#npm --prefix /home/ubuntu/G_Bot_Boilerplate/frontend run build
 
 
 
 #up containers again
 
-sudo docker-compose -f /home/ubuntu/TG_Bot_Boilerplate/docker-compose.yml up --build -d
+sudo docker compose -f /home/ubuntu/TG_Bot_Boilerplate/docker-compose.yml up --build -d
